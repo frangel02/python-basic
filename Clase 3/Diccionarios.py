5ddb86d9b926da143cf5b48b090f72856260f6ad
frangel(ClaroDom) <frangel02>
2019-03-22 13:14:39 -0400

    Diccionaries

---
+++ Clase 3/Diccionarios.py
@@ -0,0 +1,12 @@
+#Diccionario
+autores = {'libro 1': 'Jose Lujan', 'libro 2': 'Benito Juares', 'libro 3': 'Quien sabe'}
+#print(autores['libro 3'])
+print(autores.values())
+print(autores.keys())
+
+
+diccionario ={'a': (1, 2), 'b':(3, 4), 'c': (5, 6)}
+print(diccionario)
+
+for autor in autores:
+    print(autor)
